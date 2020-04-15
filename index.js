5b639316d54e9a4d86c2ecb9ee0d5b3c3bc7be0f
Layton Whiteley <layton.whiteley@gmail.com>
2020-04-15 11:39:29 +0200

    refactor!: upgrade dependencies to latest (#3)
    * refactor!: upgrade dependencies to latest
    
    * 1.0.0

--- index.js
+++ index.js
@@ -1,133 +1,140 @@
-const mime = require('mime-types')
-const through2 = require('through2')
+const mime = require('mime-types');
+const itypeof = require('itypeof');
+const through2 = require('through2');
 const duplexify = require('duplexify');
 const debug = require('debug')('mongoose-blob-store');
+const { createModel } = require('mongoose-gridfs');
 
 module.exports = function (opts) {
-    let self = {};
-    let instance;
-    const options = Object.assign({}, opts);
-    const mongooseNotConnectedErr = new Error('Mongoose not connected!');
+  let self = {};
+  let instance;
+  const options = Object.assign({}, opts);
+  const mongooseNotConnectedErr = new Error('Mongoose not connected!');
 
-    if (!options.mongooseConnection) {
-        throw new Error('options.mongooseConnection is required!');
+  if (!options.mongooseConnection) {
+    throw new Error('options.mongooseConnection is required!');
+  }
+
+  const setup = function () {
+    debug(
+      `mongoose connected, readyState(${options.mongooseConnection.readyState})`,
+    );
+    instance = createModel(options);
+    debug('mongoose blob store ready');
+  };
+
+  debug(`mongoose readyState(${options.mongooseConnection.readyState})`);
+  if (options.mongooseConnection.readyState == 1) {
+    debug('mongoose already connected, setup blob store');
+    setup();
+  } else {
+    debug('waiting on mongoose connection to setup blob store');
+    options.mongooseConnection.on('connected', setup);
+  }
+
+  self.resource = () => {
+    return self.storage();
+  };
+
+  self.storage = () => {
+    if (instance) return instance;
+    throw mongooseNotConnectedErr;
+  };
+
+  const sanitizeOpts = (opts) => {
+    if (!opts) return opts;
+
+    const type = itypeof(opts, true);
+
+    if (type === 'string' || type === 'ObjectID') {
+      opts = {
+        _id: opts,
+        key: opts.toString(),
+      };
     }
 
-    const setup = function () {
-        debug(`mongoose connected, readyState(${options.mongooseConnection.readyState})`);
-        var gridfs = require('mongoose-gridfs')(options);
-        instance = gridfs;
-        debug('mongoose blob store ready');
-    };
+    opts.filename = opts.filename || opts.key || opts.name;
+    opts.key = opts.key || opts.filename || opts.name;
+    opts._id = opts._id || opts.key || opts.filename;
+    return opts;
+  };
 
-    debug(`mongoose readyState(${options.mongooseConnection.readyState})`);
-    if (options.mongooseConnection.readyState == 1) {
-        debug('mongoose already connected, setup blob store');
-        setup();
-    } else {
-        debug('waiting on mongoose connection to setup blob store');
-        options.mongooseConnection.on('connected', setup);
+  self.createWriteStream = function (opts, cb) {
+    const proxy = through2();
+    opts = sanitizeOpts(opts);
+    var filename = opts.filename;
+    var contentType;
+    if (filename) {
+      contentType = mime.lookup(filename);
     }
-
-    self.resource = () => {
-        return self.storage().model;
-    }
-
-    self.storage = () => {
-        if (instance) return instance;
-        throw mongooseNotConnectedErr;
-    }
-
-    const sanitizeOpts = (opts) => {
-        if(!opts) return opts;
-
-        if (typeof opts === 'string'){
-            opts = {
-                key: opts,
-            };
-        }
-
-        opts.filename = opts.filename || opts.key || opts.name;
-        opts.key = opts.key || opts.filename || opts.name;
-        return opts;
-    }
-
-    self.createWriteStream = function (opts, cb) {
-        const proxy = through2();
-        opts = sanitizeOpts(opts);
-        var filename = opts.filename;
-        var contentType;
-        if (filename) {
-            contentType = mime.lookup(filename);
-        }
-        const blob = {
-            _id: opts.key,
-            filename,
-            contentType,
-            metadata: opts,
-        };
-
-        self.resource().gridfs.write(
-            blob,
-            proxy,
-            function (error, createdFile) {
-                if (error) debug('error creating file', error);
-
-                if (createdFile) {
-                    createdFile.key = createdFile._id;
-                }
-                cb(error, createdFile);
-            });
-
-        return proxy;
+    const blob = {
+      _id: opts.key,
+      filename,
+      contentType,
+      metadata: opts,
     };
 
-    self.createReadStream = function (opts) {
-        var proxy = duplexify();
-        opts = sanitizeOpts(opts);
-        
-        var stream = self.resource().readById(opts.key);
-        proxy.on('error', (err) => {
-            err.notFound = true;
-            return proxy.destroy(err);
-        });
-        proxy.setReadable(stream);
-        
-        return proxy;
-    };
+    self.resource().write(blob, proxy, function (error, createdFile) {
+      if (error) debug('error creating file', error);
 
-    self.exists = function (opts, done) {
-        var proxy = duplexify();
-        opts = sanitizeOpts(opts);
-        var stream = self.resource().readById(opts.key, (err, file) => {
-            if (err && err.message && err.message.indexOf('not opened for writing') != -1) {
-                debug('expected error when file is missing');
-                err = null;
-            }
+      if (createdFile) {
+        createdFile.key = createdFile._id;
+      }
 
-            if (err) debug('error finding file', err);
-            
-            done(err, !!file);
-        });
-        return proxy;
-    };
+      debug('.createWriteStream: createdFile', createdFile);
+      cb(error, createdFile);
+    });
 
-    self.remove = function (opts, cb) {
-        const prop = '_id';
-        opts = sanitizeOpts(opts);
-        debug('calling remove file', opts);
-        self.storage().unlink({ [prop]: opts.key }, function (error) {
-            let result;
-            if (!error) {
-                result = { id: opts.key };
-            } else {
-                debug('error removing file', error);
-            }
-            cb(error, result);
-        });
-    };
+    return proxy;
+  };
 
-    return self;
+  self.createReadStream = function (opts) {
+    var proxy = duplexify();
+    opts = sanitizeOpts(opts);
+    debug('.createReadStream: checking opts', opts);
 
+    var stream = self.resource().read(opts);
+    proxy.on('error', (err) => {
+      err.notFound = true;
+      return proxy.destroy(err);
+    });
+    proxy.setReadable(stream);
+
+    return proxy;
+  };
+
+  self.exists = function (opts, done) {
+    var proxy = duplexify();
+    opts = sanitizeOpts(opts);
+    debug('.exists: checking opts', opts);
+    self.resource().read(opts, (err, file) => {
+      if (err && err.code && err.code === 'ENOENT') {
+        debug('expected error when file is missing');
+        err = null;
+      }
+
+      if (err) {
+        debug('error finding file', err);
+      }
+
+      done(err, !!file);
+    });
+    return proxy;
+  };
+
+  self.remove = function (opts, cb) {
+    opts = sanitizeOpts(opts);
+    debug('.remove: checking opts', opts);
+    self.storage().unlink(opts, function (error) {
+      let result;
+      if (!error) {
+        result = { id: opts.key };
+      } else {
+        debug('error removing file', error);
+      }
+      cb(error, result);
+    });
+  };
+
+  return self;
 };
-
--- test.js
+++ test.js
@@ -1,34 +1,33 @@
-var fs = require('fs')
-var path = require('path')
-var test = require('tape')
-var mongoose = require('mongoose')
-var abstractBlobTests = require('abstract-blob-store/tests')
-var BlobStore = require('./')
+var test = require('tape');
+var mongoose = require('mongoose');
+var abstractBlobTests = require('abstract-blob-store/tests');
+var BlobStore = require('./');
 
-
-
-  mongoose
-  .connect('mongodb://localhost/mongoose-blob-store')
+mongoose
+  .connect('mongodb://localhost/mongoose-blob-store', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-
     test.onFinish(() => {
-        mongoose.connection.close();
-    })
+      mongoose.connection.close();
+    });
 
     const common = {
-        setup: function(t, cb) {
-          const store = BlobStore({
-            collection:'attachments',
-            model:'Attachment',
-            mongooseConnection: mongoose.connection
-          });
-          cb(null, store)
-        },
-        teardown: function(t, store, blob, cb) {
-          if (blob) store.remove(blob, cb)
-          else cb()
+      setup: function (t, cb) {
+        const store = BlobStore({
+          collection: 'attachments',
+          modelName: 'Attachment',
+          mongooseConnection: mongoose.connection,
+        });
+        cb(null, store);
+      },
+      teardown: function (t, store, blob, cb) {
+        if (blob) {
+          return store.remove(blob, cb);
         }
-      }
+        return cb();
+      },
+    };
     abstractBlobTests(test, common);
   });
-
